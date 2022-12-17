58b99c14b89abf8bf5384aeb3d85225388879427
dimaspng <d.sagachko@gmail.com>
2022-12-17 14:52:53 +0200

    Error extend, first example

--- constructor.js
+++ constructor.js
@@ -1,12 +1,33 @@
 'use strict';
 
-let set = new Set(['апельсин', 'яблоко', 'банан']);
-
-for (let value of set) {
-		alert(value);
+class ValidationError extends Error {
+		constructor(message) {
+				super(message);
+				this.name = 'ValidationError';
+		}
 }
 
-//тоже самое с foreach
-set.forEach((value, valueAgain, set) => {
-		alert(value);
-});
+function readUser(json) {
+
+		let user = JSON.parse(json);
+
+		if (!user.age) {
+				throw new ValidationError('Нет поля age');
+		}
+		if (!user.name) {
+				throw new ValidationError('Нет поля name');
+		}
+		return user;
+}
+
+try {
+		let user = readUser('{ "age": 25 }');
+} catch (err) {
+		if (err instanceof ValidationError) {
+				alert('Некоректные данные' + err.message);
+		} else if (err instanceof SyntaxError) {
+				alert('JSON ошибка синтаксиса' + err.message);
+		} else {
+				throw err;
+		}
+}
